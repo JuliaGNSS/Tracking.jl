dfff6b394a5efaf6e455cec5244532d472d23887
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-22 15:27:02 +0200

    Add descriptions

--- src/discriminators.jl
+++ src/discriminators.jl
@@ -4,12 +4,25 @@ early(x) = x[1]
 prompt(x) = x[2]
 late(x) = x[3]
 
+"""
+$(SIGNATURES)
+
+DLL Diskriminator
+Returns the calculated code offset in chips.
+"""
 function dll_disc(x)
     E = abs(early(x))
     L = abs(late(x))
     (E - L) / (E + L)
 end
 
+
+"""
+$(SIGNATURES)
+
+PLL Diskriminator
+Returns the calculated Phase error in rad.
+"""
 function pll_disc(x)
     p = prompt(x)
     atan(imag(p) / real(p))
