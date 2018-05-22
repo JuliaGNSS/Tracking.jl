b881dab39131bdb7f04d33d38e125a56f3f74069
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-22 15:37:28 +0200

    Use dll_disc where necessarry

--- test/discriminators.jl
+++ test/discriminators.jl
@@ -11,15 +11,15 @@ end
 
 
 @testset "DLL discriminator" begin
-    test_signal_very_early = [0.+0.0im,0.5+0.0im,1+0.0im]
+    test_signal_very_early = [0.+0.0im,0.5+0.0im,1.0+0.0im]
     test_signal_early = [0.25+0.0im,0.75+0.0im,0.75+0.0im]
     test_signal_in_time = [0.5+0.0im,1+0.0im,0.5+0.0im]
     test_signal_late = [0.75+0.0im,0.75+0.0im,0.25+0.0im]
-    test_signal_very_late = [1+0.0im,0.5+0.0im,0.0+0.0im]
+    test_signal_very_late = [1.0+0.0im,0.5+0.0im,0.0+0.0im]
     
-    @test Tracking.pll_disc(test_signal_very_early) == -1
-    @test Tracking.pll_disc(test_signal_early) == -0.5
-    @test Tracking.pll_disc(test_signal_in_time) == 0
-    @test Tracking.pll_disc(test_signal_late) == 0.5
-    @test Tracking.pll_disc(test_signal_very_late) == 1
+    @test Tracking.dll_disc(test_signal_very_early) == -1
+    @test Tracking.dll_disc(test_signal_early) == -0.5
+    @test Tracking.dll_disc(test_signal_in_time) == 0
+    @test Tracking.dll_disc(test_signal_late) == 0.5
+    @test Tracking.dll_disc(test_signal_very_late) == 1
 end
