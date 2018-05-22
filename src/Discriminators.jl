27c5c57f8ec8f0341bfcd44fa5d53934276b2f67
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-22 11:02:02 +0200

    Implement first Discriminators

---
+++ src/Discriminators.jl
@@ -0,0 +1,17 @@
+
+
+early(x) = x[1]
+prompt(x) = x[2]
+late(x) = x[3]
+
+function dll_disc(x)
+    E = abs(early(x))
+    L = abs(late(x))
+    (E - L) / (E + L)
+end
+
+function pll_disc(x)
+    p = prompt(x)
+    atan(imag(p), real(p))
+end
+
---
+++ test/Discriminators.jl
@@ -0,0 +1,15 @@
+using Tracking
+
+@Testset CalculateOutputErrors begin
+    testSignal = [];
+    @Testset PLL_Test begin
+        
+        @test Tracking.dll_disc(testSignal) = (abs(testSignal[1]) - abs(testSignal[3])) / (abs(testSignal[1]) + abs(testSignal[3]))
+    
+    end
+    @Testset DLL_Test begin
+        
+        @test Tracking.pll_disc(testSignal) = atan(imag(testSignal[2], real(testSignal[2])))
+    end
+end
+
