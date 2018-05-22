2ee5a5ffcb1ecc41eb6659a5d2471897046e093a
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-22 13:59:34 +0200

    refactor discriminator tests

--- test/discriminators.jl
+++ test/discriminators.jl
@@ -1,19 +1,36 @@
 
 # use the same sampelingrate and the same timestamps and just shift the values
-testSignal = [0,0.25+0.5im,0.5+1im,0.25+0,5im,0];
-testSignal_early = [0,0.15+0.3im,0.4+0.8im,0.35+0.7im,0];
-testSignal_late = [0,0.35+0,7im,0.4+0.8im,0.15+0.3im,0];
+
+
 
 
 
 @Testset "PLL discriminator test" begin
-    @test Tracking.pll_disc(testSignal) == 0.4636476090008061
+    #define testvalues
+    test_signal_prephase = [-0.5+sqrt(3)/2im, -1+sqrt(3)im, -0.5+sqrt(3)/2im]
+    test_signal = [0.5+0.0im,1+0.0im,0.5+0,0im]
+    test_signal_postphase = [0.5+sqrt(3)/2im, 1+sqrt(3)im, 0.5+sqrt(3)/2im]
+    #testing
+    @test Tracking.pll_disc(test_signal_prephase) == -pi/3  #-60°
+    @test Tracking.pll_disc(test_signal) == 0
+    @test Tracking.pll_disc(test_signal_postphase) == pi/3  #+60°
+
+
 
 end
 
 
 @Testset "DLL discriminator test" begin
-    @test Tracking.pll_disc(testSignal) == 0
-    @test Tracking.pll_disc(testSignal_early) == -0.39999999999999997
-    @test Tracking.pll_disc(testSignal_late) == -0.39999999999999997
+    #define testvalues
+    test_signal_very_early = [0.+0.0im,0.5+0.0im,1+0.0im]
+    test_signal_early = [0.25+0.0im,0.75+0.0im,0.75+0.0im]
+    test_signal_in_time = [0.5+0.0im,1+0.0im,0.5+0.0im]
+    test_signal_late = [0.75+0.0im,0.75+0.0im,0.25+0.0im]
+    test_signal_very_late = [1+0.0im,0.5+0.0im,0.0+0.0im]
+    #testing
+    @test Tracking.pll_disc(test_signal_very_early) == -1
+    @test Tracking.pll_disc(test_signal_early) == -0.5
+    @test Tracking.pll_disc(test_signal_in_time) == 0
+    @test Tracking.pll_disc(test_signal_late) == 0.5
+    @test Tracking.pll_disc(test_signal_very_late) == 1
 end
