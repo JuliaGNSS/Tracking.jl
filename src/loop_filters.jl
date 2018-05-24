eb72d5a7b3925c64945b1b5eb0a9283b76715960
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-24 15:07:45 +0200

    rename new_function to loop_filter; correct spelling mistakes; clearify parameters in function descriptions

--- src/loop_filters.jl
+++ src/loop_filters.jl
@@ -1,10 +1,10 @@
 """
 $(SIGNATURES)
 
-initialize loop_filter
-Uses the 4 matrices F,L,C,D (transition matrix, filter gain matrix and the output matrices )
-to calculate the initial state vector x and provide a first loop function
-which takes the discriminator output δΘ and returns a new loop function and the system output y
+Initialize loop_filter
+Uses the 4 matrices `F`,`L`,`C`,`D` (transition matrix, filter gain matrix and the output matrices)
+to calculate the initial state vector `x` and provide a first loop function
+which takes the discriminator output `δΘ` and returns a new loop function and the system output `y`
 """
 function init_loop_filter(F, L, C, D)
     x = zeros(size(F,1))
@@ -16,10 +16,10 @@ end
 $(SIGNATURES)
 
 loop filter
-This is the loop_filter function which will be used repeatedly after beeing initialized by init_loop_filter
-it takes the momentary state vector x, the discriminator output δΘ, and the 4 matrices F,L,C,D (transition matrix, filter gain matrix and the output matrices )
-it simulates another timestep and calculates the new state Vector x_next and the system output y
-Returns a new loop_filter_function with updated parameters, and the system output y 
+This is the loop_filter function which will be used repeatedly after being initialized by init_loop_filter
+it takes the momentary state vector `x`, the discriminator output `δΘ`, and the 4 matrices `F`,`L`,`C`,`D` (transition matrix, filter gain matrix and the output matrices)
+it simulates another timestep and calculates the new state Vector `x_next` and the system output `y`
+Returns a new loop_filter_function with updated parameters, and the system output `y`
 
 """
 function _loop_filter(x, δΘ, F, L, C, D)
@@ -32,7 +32,7 @@ end
 $(SIGNATURES)
 
 Initialize a 1st order loop_filter
-Takes the noise bandwidth and calculates the appropriate matrices for an 1st order loop_filter and hands them to init_loop_filter
+Takes the noise `bandwidth` and calculates the appropriate matrices for an 1st order loop_filter and hands them to init_loop_filter
 Returns a 1st order loop_filter function.
 """
 function init_1st_order_loop_filter(bandwidth)
@@ -48,9 +48,9 @@ end
 $(SIGNATURES)
 
 Initialize a 2nd order loop_filter
-Takes the noise bandwidth and the loop update time Δt.
+Takes the noise `bandwidth` and the loop update time `Δt`.
 Calculates the appropriate matrices for an 2nd order loop_filter and hands them to init_loop_filter
-Returns a 2nd order loop_filter fucntion.
+Returns a 2nd order loop_filter function.
 """
 function init_2nd_order_loop_filter(bandwidth,Δt)
     ω0 = bandwidth *1.89
@@ -65,9 +65,9 @@ end
 $(SIGNATURES)
 
 Initialize a 2nd order loop_filter
-Takes the noise bandwidth and the loop update time Δt.
+Takes the noise `bandwidth` and the loop update time `Δt`.
 Calculates the appropriate matrices for an 2nd order loop_filter and hands them to init_loop_filter
-Returns a 2nd order loop_filter fucntion.
+Returns a 2nd order loop_filter function.
 """
 function init_3rd_order_loop_filter(bandwidth,Δt)
     ω0 = bandwidth*1.2
