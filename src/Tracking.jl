b304b2423a49df6f6b3590bbc1c5d4255c249f82
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-24 14:04:43 +0200

    add loop_filters to the package exportations

--- src/Tracking.jl
+++ src/Tracking.jl
@@ -1,8 +1,7 @@
 module Tracking
     using DocStringExtensions
 
-    export dll_disc, pll_disc
-
+    export dll_disc, pll_disc, init_1st_order_loop_filter, init_2nd_order_loop_filter, init_3rd_order_loop_filter
     include("discriminators.jl")
     include("loop_filters.jl")
 end
