b20e4c97795ced381b78ab4be566b587adac9b64
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-24 18:13:41 +0200

    add first evaluation values to the tests

--- test/dll_and_pll.jl
+++ test/dll_and_pll.jl
@@ -1,6 +1,6 @@
 @testset "initialize PLL" begin
-    @test Tracking.init_PLL(20.0, 1023.0, 4000.0, 400000.0, 1000.0) == false
+    @test Tracking.init_PLL(20.0, 1023.0, 4000.0, 400000.0, 1000.0)[3] == 2.5955766991125455
 end
 @testset "inizialize DLL" begin
-    @test Tracking.init_DLL(20, 1023, 4000, 400000, 1,1000) == true
+    @test Tracking.init_DLL(20, 1023, 4000, 400000, 1,1000)[3] == 30.230000000000018
 end
