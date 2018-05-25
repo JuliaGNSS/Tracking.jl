6dc4b6e33ac0a1bf16fc0703ab1a0d518ec638f3
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-25 11:16:37 +0200

    Add function descriptions; change init_DLL parameter order

--- src/dll_and_pll.jl
+++ src/dll_and_pll.jl
@@ -1,11 +1,51 @@
 
+"""
+$(SIGNATURES)
 
+Initialize the locked_loop function
+Takes several inputs to initialize the _locked_loop function:
+  - the DLL or PLL discriminator function `disc`
+  - the loop filter function of 1st, 2nd or 3rd order `loop_filter`
+  - the phase calculation function `calc_phase`
+  - the signal replication function `calc_signal`
+  - the initial signal phase `init_phase`
+  - the initial signal frequency `init_freq`
+  - the loop update time intervall`Δt`
+  - the signal samlping frequency `sampling_freq`
+
+With the provided inputs and functions the replicated signal and the phase are calculated.
+
+Returns a locked_loop function, the replicated signal `init_replica`, and the calculated phase `phase` 
+
+"""
 function init_locked_loop(disc, loop_filter, calc_phase, calc_signal, init_phase, init_freq, Δt, sampling_freq)
     phase = calc_phase(Δt, init_freq, init_phase, sampling_freq)
     init_replica = calc_signal(1:Δt, init_freq, init_phase, sampling_freq)
     signal -> _locked_loop(signal, disc, loop_filter, calc_phase, calc_signal, phase, init_freq, Δt, sampling_freq), init_replica, phase 
 end
 
+
+"""
+$(SIGNATURES)
+Locked Loop function
+Takes several inputs :
+  - the replicated signal `signal`
+  - the DLL or PLL discriminator function `disc`
+  - the loop filter function of 1st, 2nd or 3rd order `loop_filter`
+  - the phase calculation function `calc_phase`
+  - the signal replication function `calc_signal`
+  - the calculated signal phase `phase`
+  - the initial signal frequency `init_freq`
+  - the loop update time intervall`Δt`
+  - the signal samlping frequency `sampling_freq`
+
+Updates its own inputs for the next time step:
+  - the replication_signal `replica`
+  - the replication signals phase `next_phase`
+  - the loop filter `next_loop_filter`
+
+Returns the updated _locked_loop function, the updated replicated signal `replica`, and the new calculated phase `phase` 
+"""
 function _locked_loop(signal, disc, loop_filter, calc_phase, calc_signal, phase, init_freq, Δt, sampling_freq)
     next_loop_filter, freq_update = loop_filter(disc(signal))
     replica = calc_signal(1:Δt, init_freq + freq_update, phase, sampling_freq)
@@ -13,11 +53,58 @@ function _locked_loop(signal, disc, loop_filter, calc_phase, calc_signal, phase,
     next_signal -> _locked_loop(next_signal, disc, next_loop_filter, calc_phase, calc_signal, next_phase, init_freq, Δt, sampling_freq), replica, phase
 end
 
+
+"""
+$(SIGNATURES)
+
+initialize PLL
+Takes several inputs:
+  - the initial signal phase `init_phase`
+  - the initial signal frequency `init_freq`
+  - the loop update time intervall`Δt`
+  - the signal samlping frequency `sampling_freq`
+  - the signal aquivalent the noise bandwidth `bandwidth`
+
+Calls the initialization of a PLL locked loop with an appropriate discriminator `disc`
+By calling init_locked_loop the following values are returned:
+ - a locked_loop function
+ - the replicated signal `init_replica`
+ - the calculated phase `phase` 
+
+"""
 function init_PLL(init_phase, init_freq, Δt, sampling_freq, bandwidth) 
   init_locked_loop(pll_disc, init_3rd_order_loop_filter(bandwidth ,Δt), GNSSSignals.get_carrier_phase, GNSSSignals.gen_carrier, init_phase, init_freq, Δt, sampling_freq)
 end
 
-function init_DLL(init_phase, init_freq, Δt, sampling_freq, sat_prn, bandwidth)
+
+"""
+$(SIGNATURES)
+
+initialize DLL
+Takes several inputs:
+  - the initial signal phase `init_phase`
+  - the initial signal frequency `init_freq`
+  - the loop update time intervall`Δt`
+  - the signal samlping frequency `sampling_freq`
+  - the signal aquivalent the noise bandwidth `bandwidth`
+  - the satellite PRN code number `sat_prn`
+
+Uses functions from the GNSSSignals module.
+
+Calls the initialization of a DLL locked loop after calculating the needed parameters:
+  - generates a sampled satellite code `code`
+  - calculates the codes phase `code_phase`
+  - creates a `calc_signal` function
+  - creates a 3rd order loop filter function loop_filter
+  - 
+
+By calling init_locked_loop the following values are returned:
+ - a locked_loop function
+ - the replicated signal `init_replica`
+ - the calculated phase `phase` 
+
+"""
+function init_DLL(init_phase, init_freq, Δt, sampling_freq, bandwidth, sat_prn)
   early_prompt_late_phase = [-0.5, 0, 0.5]
   gen_sampled_code, get_code_phase = GNSSSignals.init_gpsl1_codes()
   code = gen_sampled_code(1:Δt, init_freq, init_phase, sampling_freq, sat_prn)
--- test/dll_and_pll.jl
+++ test/dll_and_pll.jl
@@ -2,5 +2,5 @@
     @test Tracking.init_PLL(20.0, 1023.0, 4000.0, 400000.0, 1000.0)[3] == 2.5955766991125455
 end
 @testset "inizialize DLL" begin
-    @test Tracking.init_DLL(20, 1023, 4000, 400000, 1,1000)[3] == 30.230000000000018
+    @test Tracking.init_DLL(20, 1023, 4000, 400000, 1000, 1)[3] == 30.230000000000018
 end
