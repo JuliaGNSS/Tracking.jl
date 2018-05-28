24fafe63311f0ae3193ae4e67a9d2d078d7c1d06
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-28 17:54:48 +0200

    adjust loop filters so that the output is a scalar and not an 1x1 array

--- src/loop_filters.jl
+++ src/loop_filters.jl
@@ -24,7 +24,7 @@ Returns a new loop_filter_function with updated parameters, and the system outpu
 """
 function _loop_filter(x, δΘ, F, L, C, D)
     x_next = F * x + L * δΘ
-    y = C * x + D * δΘ
+    y = dot(C, x) + D * δΘ
     δΘ_next -> _loop_filter(x_next, δΘ_next, F, L, C, D), y
 end
 
@@ -39,7 +39,7 @@ function init_1st_order_loop_filter(bandwidth)
     ω0 = bandwidth * 4
     F = 0
     L = ω0
-    C = 1
+    C = [1]
     D = 0
     init_loop_filter(F, L, C, D)
 end
@@ -56,7 +56,7 @@ function init_2nd_order_loop_filter(bandwidth, Δt)
     ω0 = bandwidth * 1.89
     F = 1
     L = Δt * ω0^2
-    C = 1
+    C = [1]
     D = sqrt(2) * ω0
     init_loop_filter(F, L, C, D)
 end
@@ -73,7 +73,7 @@ function init_3rd_order_loop_filter(bandwidth, Δt)
     ω0 = bandwidth * 1.2
     F = [1 Δt; 0 1]
     L = [Δt * 1.1 * ω0^2; Δt * ω0^3]
-    C = [1 0]
-    D = [2.4 * ω0]
+    C = [1, 0]
+    D = 2.4 * ω0
     init_loop_filter(F, L, C, D)
 end
