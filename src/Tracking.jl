1dcde628581ab9ba8b0afb5262eda8ec8a2695ce
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-28 12:43:25 +0200

    include new files in the project structure

--- src/Tracking.jl
+++ src/Tracking.jl
@@ -6,4 +6,5 @@ module Tracking
     include("discriminators.jl")
     include("loop_filters.jl")
     include("dll_and_pll.jl")
+    include("tracking_loop.jl")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,4 @@ using Base.Test, Tracking
 include("discriminators.jl")
 include("loop_filters.jl")
 include("dll_and_pll.jl")
+include("tracking_loop.jl")
