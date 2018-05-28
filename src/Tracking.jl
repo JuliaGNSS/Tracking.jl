4cef91c0f144a7f243d61a45b1a703614d56e70e
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-28 13:15:37 +0200

    shorten and organice export statements

--- src/Tracking.jl
+++ src/Tracking.jl
@@ -1,7 +1,10 @@
 module Tracking
     using DocStringExtensions, GNSSSignals
 
-    export dll_disc, pll_disc,init_1st_order_loop_filter,init_2nd_order_loop_filter,init_3rd_order_loop_filter, init_PLL, init_DLL, prompt
+    export dll_disc, pll_disc, prompt #from discriminators.jl
+    export init_1st_order_loop_filter,init_2nd_order_loop_filter,init_3rd_order_loop_filter # from loop_filters.jl
+    export init_PLL, init_DLL # from dll_and_pll.jl
+    export init_tracking # from tracking_loop.jl
 
     include("discriminators.jl")
     include("loop_filters.jl")
