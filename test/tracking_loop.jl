f282d80aebee6049ccee295776b4d98bc4a0cbc7
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-29 18:01:41 +0200

    design tests for all functions in tracking_loop.jl

--- test/tracking_loop.jl
+++ test/tracking_loop.jl
@@ -1,22 +1,31 @@
 #For now
+# This takes an 3x4 Matrix as an input x
 function beamform(x)
     beamformed_x = map(x_per_phaseshift -> [0.5 0.5 0.5 0.5] * (x_per_phaseshift), x)
     hcat(beamformed_x...)
 end
 
+
+@testset "correlate_and_downconvert" begin
+    x = [[1im 2im 3im], [4im 5im 6im]]
+    replica_signal = [1im 1im 1im]
+    downconverted_signals = Tracking._downconvert(x, replica_signal)
+    @test downconverted_signals == [[1 + 0im 2 + 0im 3 + 0im], [4 + 0im 5 + 0im 6 + 0im]]
+
+    replica_codes = [[1 1 -1], [-1 -1 1]]
+    correlated_signals = Tracking._correlate(downconverted_signals, replica_codes)
+    @test correlated_signals[1][1] == [1+0im 2+0im 3+0im; 1+0im 2+0im 3+0im; -1+0im -2+0im -3+0im]
+end
+
 @testset "Tracking_loop" begin
-    incoming_signals = [cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π), cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π), cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π), cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π)]
-    correlator_output = [0.5 + 0.0im 1.0 + 0.0im 0.5 + 0.0im]
+    test_signal = cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π)
+    incoming_signals = [test_signal, test_signal, test_signal, test_signal]
     tracking_loop = Tracking.init_tracking(Tracking.init_PLL, Tracking.init_DLL, 1/3 * π, 50, 2.0, 1023e3, 1e-3, 4e6, beamform, 4000, 18.0, 1.0, 1)
     next_tracking_loop, code_phase, prompt_correlated_signal, prompt_beamformed_signal = tracking_loop(incoming_signals)
-    @test code_phase == 20
-    @test prompt_correlated_signal == 1
-    @test prompt_beamformed_signal == 2
-    """
-    next_tracking_loop, code_phase, prompt_correlated_signal, prompt_beamformed_signal = next_tracking_loop(incoming_signal)
-    println("Next evaluation step -------------------------------------------------------------")
-    @test code_phase == 20
-    @test prompt_correlated_signal == 1
-    @test prompt_beamformed_signal == 2
-    """
+    @test code_phase ≈ 2.0
+    @test prompt_beamformed_signal == beamform([prompt_correlated_signal,prompt_correlated_signal,prompt_correlated_signal])[2]
+
+    next_tracking_loop, code_phase, prompt_correlated_signal, prompt_beamformed_signal = next_tracking_loop(incoming_signals)
+    @test code_phase ≈ 2.00096
+    @test prompt_beamformed_signal == beamform([prompt_correlated_signal,prompt_correlated_signal,prompt_correlated_signal])[2]
 end
