0670bbe27612a76907ce81d5d1ce04182ed6d5a4
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-05-29 16:17:02 +0200

    actually include the files from the commit before

--- src/tracking_loop.jl
+++ src/tracking_loop.jl
@@ -1,24 +1,23 @@
-# For now
 function downconvert(x, replica)
-    x .* conj(replica)
+    map(signal -> signal .* conj(replica), x)
 end
 
-function correlate(x, replica)
-    replica' * x
+function correlate(x, replicas)
+    map(replica -> map(single_antenna_signal -> replica' * single_antenna_signal, x), replicas)
 end
 
 
-function init_tracking(init_PLL, init_DLL, init_carrier_phase, init_carrier_freq, init_code_phase, init_code_freq, Δt, f_s, beamform, n_samples, pll_disc_bandwidth, dll_disc_bandwidth, sat_prn)
-    PLL, init_carrier_replica = init_PLL(init_carrier_phase, init_carrier_freq, n_samples,  f_s, pll_disc_bandwidth, Δt)
-    DLL, init_code_replica = init_DLL(init_code_phase, init_code_freq, n_samples,  f_s, dll_disc_bandwidth, Δt, sat_prn)
-    signal -> _tracking(signal, PLL, DLL, init_carrier_phase, init_carrier_freq, init_code_phase, init_code_freq, Δt, f_s, beamform, n_samples, pll_disc_bandwidth, dll_disc_bandwidth, sat_prn)
+function init_tracking(init_PLL, init_DLL, init_carrier_phase, init_carrier_freq, init_code_phase, init_code_freq, Δt, f_s, beamform, num_samples, pll_disc_bandwidth, dll_disc_bandwidth, sat_prn)
+    PLL, init_carrier_replica = init_PLL(init_carrier_phase, init_carrier_freq, num_samples,  f_s, pll_disc_bandwidth, Δt)
+    DLL, init_code_replicas = init_DLL(init_code_phase, init_code_freq, num_samples,  f_s, dll_disc_bandwidth, Δt, sat_prn)
+    signals -> _tracking(signals, PLL, DLL, init_carrier_replica, init_code_replicas, beamform)
 end
 
-function _tracking(signal, PLL, DLL, carrier_replica, code_replica, beamform)
-    downconverted_signal = downconvert(signal, carrier_replica)
-    correlated_signal = correlate(downconverted_signal, code_replica)
-    beamformed_signal = beamform(correlated_signal)
+function _tracking(signals, PLL, DLL, carrier_replica, code_replicas, beamform)
+    downconverted_signals = downconvert(signals, carrier_replica)
+    correlated_signals = correlate(downconverted_signals, code_replicas)
+    beamformed_signal = beamform(correlated_signals)
     next_PLL, next_carrier_replica = PLL(beamformed_signal)
     next_DLL, next_code_replicas, code_phase = DLL(beamformed_signal)
-    next_signal -> _tracking(next_signal, next_PLL, next_DLL, next_carrier_replica, next_code_replicas, beamform), code_phase , prompt(correlated_signal), prompt(beamformed_signal) 
+    next_signal -> _tracking(next_signal, next_PLL, next_DLL, next_carrier_replica, next_code_replicas, beamform), code_phase, prompt(correlated_signals), prompt(beamformed_signal) 
 end
--- test/tracking_loop.jl
+++ test/tracking_loop.jl
@@ -1,11 +1,22 @@
+#For now
 function beamform(x)
-    sum(x)
+    beamformed_x = map(x_per_phaseshift -> [0.5 0.5 0.5 0.5] * (x_per_phaseshift), x)
+    hcat(beamformed_x...)
 end
 
 @testset "Tracking_loop" begin
-    tracking_loop, code_phase, prompt_correlated_signal, prompt_beamformed_signal = Tracking.init_tracking(init_PLL, init_DLL, 20.0, 50, 20.0, 1023e3, 1e-3, 4e6, beamform, 4000, 18.0, 1.0, 1)
+    incoming_signals = [cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π), cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π), cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π), cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π)]
+    correlator_output = [0.5 + 0.0im 1.0 + 0.0im 0.5 + 0.0im]
+    tracking_loop = Tracking.init_tracking(Tracking.init_PLL, Tracking.init_DLL, 1/3 * π, 50, 2.0, 1023e3, 1e-3, 4e6, beamform, 4000, 18.0, 1.0, 1)
+    next_tracking_loop, code_phase, prompt_correlated_signal, prompt_beamformed_signal = tracking_loop(incoming_signals)
     @test code_phase == 20
-    @test prompt_correlated_signal = 1
-    @test prompt_beamformed_signal = 2
-    @test tracking_loop([1 3 45 6 72  34 7])
+    @test prompt_correlated_signal == 1
+    @test prompt_beamformed_signal == 2
+    """
+    next_tracking_loop, code_phase, prompt_correlated_signal, prompt_beamformed_signal = next_tracking_loop(incoming_signal)
+    println("Next evaluation step -------------------------------------------------------------")
+    @test code_phase == 20
+    @test prompt_correlated_signal == 1
+    @test prompt_beamformed_signal == 2
+    """
 end
