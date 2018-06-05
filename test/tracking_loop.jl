38fd3bb1380f7da916963bd1a9cb04b474c24e35
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-06-05 15:40:10 +0200

    add spaces

--- test/tracking_loop.jl
+++ test/tracking_loop.jl
@@ -20,7 +20,7 @@ end
 end
 @testset "Tracking_loop" begin
     test_signal = cis.(2 * π * 50 / 4e6 * (1:4000) + 1 / 3 * π)
-    samples_code = GNSSSignals.gen_sat_code(1:4000,1.023e6,2.0,4e6,SATELLITE_1_CODE)
+    samples_code = GNSSSignals.gen_sat_code(1:4000, 1.023e6, 2.0, 4e6, SATELLITE_1_CODE)
     incoming_signals = [1,1,1,1] .* (test_signal .* samples_code)'
     tracking_loop = Tracking.init_tracking(1/3 * π, 50, 2.0, 1023e3, 1e-3, 4e6, beamform, 18.0, 1.0, 1)
     next_tracking_loop, code_phase, prompts_correlated_signals, prompt_beamformed_signal = tracking_loop(incoming_signals)
