bb4cc1ad61c1dcb048b67c990eced9d09d2290a4
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-06-08 18:08:25 +0200

    shift samples instead of chip phase

--- src/dll_and_pll.jl
+++ src/dll_and_pll.jl
@@ -96,10 +96,10 @@ Initialize a dll_locked_loop function by calculating the needed parameters and r
 """
 function init_DLL(init_phase, init_freq, sampling_freq, bandwidth, Δt, sat_prn)
   early_prompt_late_phase = [-0.5, 0.0, 0.5]
-  gen_sampled_code, get_code_phase = GNSSSignals.init_gpsl1_codes()
-  calc_signal(samples, f, phase, sampling_freq) = map(phase_shift -> gen_sampled_code(samples, f, phase + phase_shift, sampling_freq, sat_prn), early_prompt_late_phase)
-  loop_filter = init_2nd_order_loop_filter(bandwidth, Δt)
   num_samples = sampling_freq * Δt
+  gen_sampled_code, get_code_phase = GNSSSignals.init_gpsl1_codes()
+  calc_signal(samples, f, phase, sampling_freq) = map(sample_shift -> gen_sampled_code(minimum(samples)+sample_shift:maximum(samples)+sample_shift, f, phase, sampling_freq, sat_prn), early_prompt_late_phase .* round(Int, sampling_freq / f))
+  loop_filter = init_2nd_order_loop_filter(bandwidth, Δt)
   init_locked_loop(dll_disc, loop_filter, get_code_phase, calc_signal, init_phase, init_freq, sampling_freq, num_samples)
 end
 
