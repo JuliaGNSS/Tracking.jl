6f767a4b26a78fb792e4519005219fff9b8a6872
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-06-08 11:26:58 +0200

    reduce amount of code; add frequency tests

--- test/tracking_loop.jl
+++ test/tracking_loop.jl
@@ -21,20 +21,16 @@ end
 @testset "Tracking_loop" begin
     test_signal = cis.(2 * π * 50 / 4e6 * (1:32000) + 1 / 3 * π)
     samples_code = GNSSSignals.gen_sat_code(1:32000, 1.023e6, 2.0, 4e6, SATELLITE_1_CODE) * 10^(-20/20)
-    incoming_signals1 = [1,1,1,1] .* (test_signal[1:4000] .* samples_code[1:4000])'
-    incoming_signals2 = [1,1,1,1] .* (test_signal[4001:8000] .* samples_code[4001:8000])'
-    incoming_signals3 = [1,1,1,1] .* (test_signal[8001:12000] .* samples_code[8001:12000])'
+    incoming_signals = [1,1,1,1] .* (test_signal[1:4000] .* samples_code[1:4000])'
     tracking_loop = Tracking.init_tracking(1/3 * π, 50, 2.0, 1023e3, 1e-3, 4e6, beamform, 18.0, 1.0, 1, scale_factor)
-    next_tracking_loop, code_phase, prompts_correlated_signals = tracking_loop(incoming_signals1)
-    @test code_phase ≈ 2.0 atol = 0.001
-    next_tracking_loop, code_phase, prompts_correlated_signals = next_tracking_loop(incoming_signals2)
-    @test code_phase ≈ 2.0 atol = 0.001
-    next_tracking_loop, code_phase, prompts_correlated_signals = next_tracking_loop(incoming_signals3)
-    @test code_phase ≈ 2.0 atol = 0.001
-    for i = 0:4
-        incoming_signals = [1,1,1,1] .* (test_signal[(12001 +4000*i):(16000+4000*i)] .* samples_code[(12001+4000*i):(16000+4000*i)])'
-        next_tracking_loop, code_phase, prompts_correlated_signals = next_tracking_loop(incoming_signals)
-        @test code_phase ≈ 2.0 atol = 0.001
+    next_tracking_loop, code_phase, prompts_correlated_signals, frequency_update = tracking_loop(incoming_signals)
+    @test code_phase ≈ 2.0 atol = 1e-3
+    @test frequency_update ≈ 0.0 atol = 1e-13
+    for i = 0:6
+        incoming_signals = [1,1,1,1] .* (test_signal[(4001 +4000*i):(8000+4000*i)] .* samples_code[(4001+4000*i):(8000+4000*i)])'
+        next_tracking_loop, code_phase, prompts_correlated_signals, frequency_update = next_tracking_loop(incoming_signals)
+        @test code_phase ≈ 2.0 atol = 1e-3
+        @test frequency_update ≈ 0.0 atol = 1e-13       
     end
 
 end
