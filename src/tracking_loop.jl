4365234e68cd063214b006a8c623a3adb3fdfd1d
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-07-26 12:20:03 +0200

    start implementing new joined tracknig algorithm

--- src/tracking_loop.jl
+++ src/tracking_loop.jl
@@ -59,6 +59,16 @@ function init_tracking(init_carrier_phase, carrier_freq, interm_freq, init_carri
     (signals, beamform, velocity_aiding = 0.0) -> _tracking(signals, beamform, gen_carrier_replica, gen_code_replica, interm_freq + init_carrier_doppler, 0.0, code_freq + code_doppler, 0.0, carrier_loop, code_loop, aiding_scale_factor, velocity_aiding)
 end
 
+function init_joined_tracking(init_carrier_phase, carrier_freq, interm_freq, init_carrier_doppler, init_code_phase, code_freq, sampling_freq, pll_disc_bandwidth, dll_disc_bandwidth, sat_svid, gen_sampled_code, get_code_phase)
+    gen_code_replica = init_code_replica(init_code_phase, sampling_freq, sat_svid, gen_sampled_code, get_code_phase)
+    gen_carrier_replica = init_carrier_replica(init_carrier_phase, sampling_freq)
+    carrier_loop = init_carrier_loop(pll_disc_bandwidth, sampling_freq)
+    code_loop = init_code_loop(dll_disc_bandwidth, sampling_freq)
+    aiding_scale_factor = code_freq / carrier_freq
+    code_doppler = init_carrier_doppler * aiding_scale_factor
+    (signals, beamform, velocity_aiding = 0.0) -> _tracking(signals, beamform, gen_carrier_replica, gen_code_replica, interm_freq + init_carrier_doppler, 0.0, code_freq + code_doppler, 0.0, carrier_loop, code_loop, aiding_scale_factor, velocity_aiding)
+end
+
 """
 $(SIGNATURES)
 
@@ -80,16 +90,43 @@ function _tracking(signals, beamform, gen_carrier_replica, gen_code_replica, ini
     (next_signal, beamform, next_velocity_aiding = 0.0) -> _tracking(next_signal, beamform, next_gen_carrier_replica, next_gen_code_replica, init_carrier_freq, next_carrier_doppler, init_code_freq, next_code_doppler, next_carrier_loop, next_code_loop, aiding_scale_factor, velocity_aiding), code_phase, prompt(correlated_signals), init_carrier_freq + carrier_doppler
 end
 
-function init_carrier_loop(bandwidth, sampling_freq)
-    (correlator_output, num_samples) -> _loop(correlator_output, num_samples, pll_disc, init_3rd_order_loop_filter(bandwidth), sampling_freq)
+function _joined_tracking(l1_signals, l5_signals, beamform, gen_carrier_replica, gen_code_replica, l1_init_carrier_freq, l5_init_carrier_freq, carrier_doppler, init_code_freq, code_doppler, carrier_loop, code_loop, aiding_scale_factor, velocity_aiding)
+    l1_num_samples = size(l1_signals, 2)
+    l5_num_samples = size(l5_signals, 2)
+    l1_next_gen_carrier_replica, l1_carrier_replica, l1_carrier_phase = gen_carrier_replica(l1_num_samples, l1_init_carrier_freq + carrier_doppler)
+    l5_next_gen_carrier_replica, l5_carrier_replica, l5_carrier_phase = gen_carrier_replica(l5_num_samples, l5_init_carrier_freq + carrier_doppler) #scale factor for carrier_doppler
+
+    # output mit sqrt (115/154) an die beiden replicas der pll geben
+    l1_next_gen_code_replica, l1_code_replicas, l1_code_phase = gen_code_replica(num_samples, init_code_freq + code_doppler)
+    l5_next_gen_code_replica, l5_code_replicas, l5_code_phase = gen_code_replica(num_samples, init_code_freq + code_doppler)
+
+    # outputs der dll mit sqrt(10) und an die correlation geben
+    l1_downconverted_signals = downconvert(l1_signals, l1_carrier_replica') # hier mit carrier_replica 2* gewichtet replizieren
+    l5_downconverted_signals = downconvert(l5_signals, l5_carrier_replica') # hier mit carrier_replica 2* gewichtet replizieren
+
+    l1_correlated_signals = map(replica -> correlate(l1_downconverted_signals, replica), l1_code_replicas) # jetzt 2 mal correlieren mit den beiden gewichteten dlls
+    l5_correlated_signals = map(replica -> correlate(l5_downconverted_signals, replica), l5_code_replicas) # jetzt 2 mal correlieren mit den beiden gewichteten dlls
+    
+    #ToDo beamform with PLL to filter data, for not simulated signals
+    
+    correlated_signals = vcat(l1_correlated_signals, l5_correlated_signals)
+    beamformed_signal = hcat(map(beamform, correlated_signals)...) # beamform 2* 2signals into 1 signal and give that to the loops.
+    next_carrier_loop, carrier_freq_update = carrier_loop(beamformed_signal, Δt) #pll  #zu 
+    next_code_loop, code_freq_update = code_loop(beamformed_signal, Δt) #dll
+    next_carrier_doppler = carrier_freq_update + velocity_aiding    #doppler ist unterschiedlich muss dann gewichtet werden-> wird später gewichtet?
+    next_code_doppler = code_freq_update + next_carrier_doppler * aiding_scale_factor  #doppler ist unterschiedlich muss dann gewichtet werden?
+    (next_signal, beamform, next_velocity_aiding = 0.0) -> _tracking(next_signal, beamform, next_gen_carrier_replica, next_gen_code_replica, init_carrier_freq, next_carrier_doppler, init_code_freq, next_code_doppler, next_carrier_loop, next_code_loop, aiding_scale_factor, velocity_aiding), code_phase, prompt(correlated_signals), init_carrier_freq + carrier_doppler
 end
 
-function init_code_loop(bandwidth, sampling_freq)
-    (correlator_output, num_samples) -> _loop(correlator_output, num_samples, dll_disc, init_2nd_order_loop_filter(bandwidth), sampling_freq)
+function init_carrier_loop(bandwidth, Δt)
+    (correlator_output, Δt) -> _loop(correlator_output, pll_disc, init_3rd_order_loop_filter(bandwidth), Δt)
 end
 
-function _loop(correlator_output, num_samples, disc, loop_filter, sampling_freq)
-    Δt = num_samples / sampling_freq
+function init_code_loop(bandwidth, Δt)
+    (correlator_output, Δt) -> _loop(correlator_output, dll_disc, init_2nd_order_loop_filter(bandwidth), Δt)
+end
+
+function _loop(correlator_output, disc, loop_filter, Δt)
     next_loop_filter, freq_update = loop_filter(disc(correlator_output), Δt)
-    (next_correlator_output, next_num_samples) -> _loop(next_correlator_output, next_num_samples, disc, next_loop_filter, sampling_freq), freq_update
+    (next_correlator_output, Δt) -> _loop(next_correlator_output, disc, next_loop_filter, Δt), freq_update
 end
--- test/tracking_loop.jl
+++ test/tracking_loop.jl
@@ -35,3 +35,22 @@ end
     end
 
 end
+    @testset "Joined TrackingLoop" begin
+    l1_carrier = cis.(2 * π * 50 / 4e6 * (1:32000) + 1 / 3 * π)
+    l5_carrier = cis.(2 * π * 50 / 4e7 * (1:320000) + 1 /3 * π)
+    l1_sampled_code = GNSSSignals.gen_sat_code(1:32000, 1.023e6, 2.0, 4e6, SATELLITE_1_CODE) * 10^(-20/20)
+    l1_sampled_code = GNSSSignals.gen_sat_code(1:32000, 1.023e6, 2.0, 4e6, SATELLITE_1_CODE) * 10^(-20/20)
+
+    incoming_signals = [1, 1, 1, 1] .* (carrier[1:4000] .* sampled_code[1:4000])'
+    gen_sampled_code, get_code_phase = init_gpsl1_codes()
+    track = Tracking.init_tracking(1/3 * π, 1575.43e6, 50, 0.0, 2.0, 1.023e6, 4e6, 18.0, 1.0, 1, gen_sampled_code, get_code_phase)
+    next_track, code_phase, prompts_correlated_signals, carrier_freq = track(incoming_signals, beamform)
+    @test code_phase ≈ 2.0 atol = 1e-3
+    @test carrier_freq ≈ 50.0 atol = 1e-13
+    for i = 0:6
+        incoming_signals = [1, 1, 1, 1] .* (carrier[(4001 + 4000 * i):(8000 + 4000 * i)] .* sampled_code[(4001 + 4000 * i):(8000 + 4000 * i)])'
+        next_track, code_phase, prompts_correlated_signals, carrier_freq = next_track(incoming_signals, beamform)
+        @test code_phase ≈ 2.0 atol = 1e-3
+        @test carrier_freq ≈ 50.0 atol = 1e-13       
+    end 
+end
