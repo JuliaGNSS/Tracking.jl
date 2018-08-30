6c07c9fffdbf69e40f3ab86e59a277b7101c1ace
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2018-08-30 13:00:19 +0200

    Joined tracking can process more than 2 signals. Warning: Does not work when signals have the same power

--- src/joined_tracking.jl
+++ src/joined_tracking.jl
@@ -4,52 +4,66 @@ function init_joined_tracking(l1_system::GPSL1, l5_system::GPSL5, l1_results::Tr
     l5_gen_code_replica = init_code_replica(l5_system, l5_system.code_freq + l5_results.code_doppler, l5_results.code_phase, l5_sampling_freq, sat_prn)
     l1_gen_carrier_replica = init_carrier_replica(l1_interm_freq + l1_results.carrier_doppler, l1_results.carrier_phase, l1_sampling_freq)
     l5_gen_carrier_replica = init_carrier_replica(l5_interm_freq + l5_results.carrier_doppler, l5_results.carrier_phase, l5_sampling_freq)
-    carrier_loop = init_carrier_loop(pll_bandwidth) # pll
-    code_loop = init_code_loop(dll_bandwidth) # dll
+    carrier_loop = init_carrier_loop(pll_bandwidth)
+    code_loop = init_code_loop(dll_bandwidth)
     (l1_signals, l5_signals, beamform, velocity_aiding = 0.0) -> _joined_tracking(l1_signals, l5_signals, beamform, l1_gen_carrier_replica, l5_gen_carrier_replica, l1_gen_code_replica, l5_gen_code_replica, #=carrier_freq_update=# 0.0, #=code_freq_update=# 0.0, carrier_loop, code_loop, #=l1_aiding_scale_factor=# l1_system.code_freq / l1_system.center_freq, #=l5_aiding_scale_factor=# l5_system.code_freq / l5_system.center_freq, velocity_aiding, l1_sampling_freq)
 end
 
-#= function init_joined_tracking(systems::Vector{GNSSSystem}, tracking_results::Vector{TrackingResults}, interm_freqs, pll_bandwidth, dll_bandwidth, sat_svid)
-    gen_code_replicas = map((system, track_results) -> init_code_replica(track_results.code_phase, system.sampling_freq, sat_svid, system.gen_sampled_code, system.calc_next_code_phase), systems, tracking_results)
-    gen_carrier_replicas = map((system, track_results) -> init_carrier_replica(track_results.carrier_phase, system.sampling_freq), systems, tracking_results)
+function init_joined_tracking(systems::Vector{T}, tracking_results::Vector{TrackingResults}, sample_freqs, interm_freqs, pll_bandwidth, dll_bandwidth, sat_prn) where T <: AbstractGNSSSystem
+    gen_code_replicas = map((system, track_result, sample_freq) -> init_code_replica(system, system.code_freq + track_result.code_doppler, track_result.code_phase, sample_freq, sat_prn), systems, tracking_results, sample_freqs)
+    gen_carrier_replicas = map((track_result, interm_freq, sample_freq) -> init_carrier_replica(interm_freq + track_result.carrier_doppler, track_result.carrier_phase, sample_freq), tracking_results, interm_freqs, sample_freqs)
     carrier_loop = init_carrier_loop(pll_bandwidth)
     code_loop = init_code_loop(dll_bandwidth)
-    (l1_signals, l5_signals, beamform, velocity_aiding = 0.0) -> _joined_tracking(l1_signals, l5_signals, beamform, l1_gen_carrier_replica, l5_gen_carrier_replica, l1_gen_code_replica, l5_gen_code_replica, l1_init_carrier_freq + l1_results.carrier_doppler, l5_init_carrier_freq + l5_results.carrier_doppler, l1_system.code_freq + l1_results.code_doppler, l5_system.code_freq + l5_results.code_doppler, 0.0, 0.0, carrier_loop, code_loop, l1_system.code_freq / l1_system.center_freq, l5_system.code_freq / l5_system.center_freq, velocity_aiding, l1_sampling_freq)
-end =#
+    (signals, beamform, velocity_aidings = zeros(length(systems))) -> _joined_tracking(systems, signals, sample_freqs, beamform, gen_carrier_replicas, gen_code_replicas, 0.0, 0.0, carrier_loop, code_loop, velocity_aidings)
+end
 
-#= function _joined_tracking(signals, systems, beamform, gen_carrier_replicas, gen_code_replicas, init_carrier_freqs, init_code_freqs, carrier_freq_update, code_freq_update, carrier_loop, code_loop, velocity_aidings)
-    l1_num_samples = size(l1_signals, 1)
-    l5_num_samples = size(l5_signals, 1)
-    Δt =  l1_num_samples / l1_sampling_freq
-    l1_carrier_doppler = (carrier_freq_update + velocity_aiding) / sqrt(115/154)
-    l5_carrier_doppler = (carrier_freq_update + velocity_aiding) * sqrt(115/154)
-    l1_code_doppler = code_freq_update / sqrt(10) + l1_carrier_doppler *  l1_aiding_scale_factor
-    l5_code_doppler = code_freq_update * sqrt(10) + l5_carrier_doppler *  l5_aiding_scale_factor
+function gen_replica_downconvert_correlate(system, signal, sample_freq, gen_carrier_replica, gen_code_replica, carrier_freq_update, code_freq_update, center_freq_geometric_mean, code_freq_geometric_mean, velocity_aiding)
+    num_samples = size(signal, 1)
+    carrier_doppler = carrier_freq_update * system.center_freq / center_freq_geometric_mean + velocity_aiding
+    code_doppler = code_freq_update * system.code_freq / code_freq_geometric_mean + carrier_doppler * system.code_freq / system.center_freq
 
-    next_l1_gen_carrier_replica, l1_carrier_replica, l1_carrier_phase = l1_gen_carrier_replica(l1_num_samples, l1_init_carrier_freq + l1_carrier_doppler)
-    next_l5_gen_carrier_replica, l5_carrier_replica, l5_carrier_phase = l5_gen_carrier_replica(l5_num_samples, l5_init_carrier_freq + l5_carrier_doppler) #scale factor for carrier_doppler
+    next_gen_carrier_replica, carrier_replica, next_carrier_phase = gen_carrier_replica(num_samples, carrier_doppler)
+    next_gen_code_replica, code_replicas, next_code_phase = gen_code_replica(num_samples, code_doppler)
 
-    next_l1_gen_code_replica, l1_code_replicas, next_l1_code_phase = l1_gen_code_replica(l1_num_samples, l1_init_code_freq + l1_code_doppler)
-    next_l5_gen_code_replica, l5_code_replicas, next_l5_code_phase = l5_gen_code_replica(l5_num_samples, l5_init_code_freq + l5_code_doppler)
+    downconverted_signal = downconvert(signal, carrier_replica)
+    correlated_signals = map(replica -> correlate(downconverted_signal, replica).', code_replicas)
 
-    l1_downconverted_signals = downconvert(l1_signals, l1_carrier_replica)
-    l5_downconverted_signals = downconvert(l5_signals, l5_carrier_replica)
-    #println("downconverted: l1: ", size(l1_downconverted_signals), " l5: ", size(l5_downconverted_signals))
-    l1_correlated_signals = map(replica -> correlate(l1_downconverted_signals, replica).', l1_code_replicas)
-    l5_correlated_signals = map(replica -> correlate(l5_downconverted_signals, replica).', l5_code_replicas)
-    #println("corr signals: l1:", l1_correlated_signals, " l5: ", l5_correlated_signals)
-    #ToDo beamform with PLL to filter data, for not simulated signals
-    correlated_signals = vcat(hcat(l1_correlated_signals...), hcat(l5_correlated_signals...))
-    #println("combined correlated", correlated_signals)
-    beamformed_signal = beamform(correlated_signals)
-    #println("beamformed_signal: ", beamformed_signal)
-    next_carrier_loop, next_carrier_freq_update = carrier_loop(beamformed_signal, Δt) #pll 
-    next_code_loop, next_code_freq_update = code_loop(beamformed_signal, Δt) #dll
-    l1_results = TrackingResults(l1_carrier_doppler, l1_carrier_phase, l1_code_doppler, next_l1_code_phase, prompt(l1_correlated_signals))
-    l5_results = TrackingResults(l5_carrier_doppler, l5_carrier_phase, l5_code_doppler, next_l5_code_phase, prompt(l5_correlated_signals))
-    joined_results = JoinedTrackingResults(l1_results, l5_results)
-    (next_l1_signals, next_l5_signals, beamform, next_velocity_aiding = 0.0) -> _joined_tracking(next_l1_signals, next_l5_signals, beamform, next_l1_gen_carrier_replica, next_l5_gen_carrier_replica, next_l1_gen_code_replica, next_l5_gen_code_replica, l1_init_carrier_freq, l5_init_carrier_freq, l1_init_code_freq, l5_init_code_freq, next_carrier_freq_update, next_code_freq_update, next_carrier_loop, next_code_loop, l1_aiding_scale_factor, l5_aiding_scale_factor, next_velocity_aiding, l1_sampling_freq), joined_results
-end =#
+    tracking_result = TrackingResults(carrier_doppler, next_carrier_phase, code_doppler, next_code_phase, prompt(correlated_signals))
+
+    tracking_result, reduce(hcat, correlated_signals) ./ num_samples, next_gen_carrier_replica, next_gen_code_replica
+end
+
+function _joined_tracking(systems, signals, sample_freqs, beamform, gen_carrier_replicas, gen_code_replicas, carrier_freq_update, code_freq_update, carrier_loop, code_loop, velocity_aidings)
+    num_systems = length(systems)
+    num_samples = map(signal -> size(signal, 1), signals)
+    num_ants = sum(map(signal -> size(signal, 2), signals))
+    Δts = map((num_samples, sample_freq) -> num_samples / sample_freq, num_samples, sample_freqs)
+    if !all(Δts .== Δts[1])
+        error("Signal time interval must be identical for all given signals.")
+    end
+    Δt = Δts[1]
+
+    center_freq_geometric_mean = prod([system.center_freq for system in systems])^(1 / num_systems)
+    code_freq_geometric_mean = prod([system.code_freq for system in systems])^(1 / num_systems)
+    
+    intermediate_results = map(systems, signals, sample_freqs, gen_carrier_replicas, gen_code_replicas, velocity_aidings) do system, signal, sample_freq, gen_carrier_replica, gen_code_replica, velocity_aiding
+        gen_replica_downconvert_correlate(system, signal, sample_freq, gen_carrier_replica, gen_code_replica, carrier_freq_update, code_freq_update, center_freq_geometric_mean, code_freq_geometric_mean, velocity_aiding)
+    end
+    tracking_results = [intermediate_result[1] for intermediate_result in intermediate_results]
+    correlated_signals = [intermediate_result[2] for intermediate_result in intermediate_results]
+    next_gen_carrier_replicas = [intermediate_result[3] for intermediate_result in intermediate_results]
+    next_gen_code_replicas = [intermediate_result[4] for intermediate_result in intermediate_results]
+
+    joined_correlated_signals = reduce(vcat, correlated_signals)
+    #show(STDOUT, "text/plain", joined_correlated_signals)
+    #error("test")
+    beamformed_signal = beamform(joined_correlated_signals)
+
+    next_carrier_loop, next_carrier_freq_update = carrier_loop(beamformed_signal, Δt)
+    next_code_loop, next_code_freq_update = code_loop(beamformed_signal, Δt)
+
+    (next_signals, next_beamform, next_velocity_aidings = zeros(length(systems))) -> _joined_tracking(systems, next_signals, sample_freqs, next_beamform, next_gen_carrier_replicas, next_gen_code_replicas, next_carrier_freq_update, next_code_freq_update, next_carrier_loop, next_code_loop, next_velocity_aidings), tracking_results
+end
 
 function _joined_tracking(l1_signals, l5_signals, beamform, l1_gen_carrier_replica, l5_gen_carrier_replica, l1_gen_code_replica, l5_gen_code_replica, carrier_freq_update, code_freq_update, carrier_loop, code_loop, l1_aiding_scale_factor, l5_aiding_scale_factor, velocity_aiding, l1_sampling_freq)
     l1_num_samples = size(l1_signals, 1)
@@ -82,5 +96,5 @@ function _joined_tracking(l1_signals, l5_signals, beamform, l1_gen_carrier_repli
     l5_results = TrackingResults(l5_carrier_doppler, next_l5_carrier_phase, l5_code_doppler, next_l5_code_phase, prompt(l5_correlated_signals))
     joined_results = JoinedTrackingResults(l1_results, l5_results)
 
-    (next_l1_signals, next_l5_signals, beamform, next_velocity_aiding = 0.0) -> _joined_tracking(next_l1_signals, next_l5_signals, beamform, next_l1_gen_carrier_replica, next_l5_gen_carrier_replica, next_l1_gen_code_replica, next_l5_gen_code_replica, next_carrier_freq_update, next_code_freq_update, next_carrier_loop, next_code_loop, l1_aiding_scale_factor, l5_aiding_scale_factor, next_velocity_aiding, l1_sampling_freq), joined_results
+    (next_l1_signals, next_l5_signals, next_beamform, next_velocity_aiding = 0.0) -> _joined_tracking(next_l1_signals, next_l5_signals, next_beamform, next_l1_gen_carrier_replica, next_l5_gen_carrier_replica, next_l1_gen_code_replica, next_l5_gen_code_replica, next_carrier_freq_update, next_code_freq_update, next_carrier_loop, next_code_loop, l1_aiding_scale_factor, l5_aiding_scale_factor, next_velocity_aiding, l1_sampling_freq), joined_results
 end
--- test/joined_tracking.jl
+++ test/joined_tracking.jl
@@ -19,7 +19,7 @@
     l1_code_doppler = l1_doppler * l1_code_freq / l1_center_freq
     l5_code_doppler = l5_doppler * l5_code_freq / l5_center_freq
     l1_code_phase = 2.0
-    l5_code_phase = 0.0
+    l5_code_phase = 4.0
 
     run_time = 1
     integration_time = 1e-3
@@ -85,3 +85,91 @@
     figure("L5 code dopplers")
     plot(l5_code_dopplers) =#
 end
+
+#= @testset "Joined tracking" begin
+
+    function beamform(x)
+        [0.5 0.5 0.5 0.5] * x
+    end
+
+    l1_center_freq = 1.57542e9
+    l5_center_freq = 1.17645e9
+    l1_code_freq = 1023e3
+    l5_code_freq = 1023e4
+    l1_doppler = 10
+    l5_doppler = l1_doppler * l5_center_freq / l1_center_freq
+    l1_interm_freq = 50
+    l5_interm_freq = 100
+    l1_carrier_phase = π / 3
+    l5_carrier_phase = π / 4
+    l1_sample_freq = 4e6
+    l5_sample_freq = 4e7
+    l1_code_doppler = l1_doppler * l1_code_freq / l1_center_freq
+    l5_code_doppler = l5_doppler * l5_code_freq / l5_center_freq
+    l1_code_phase = 2.0
+    l5_code_phase = 4.0
+
+    run_time = 1
+    integration_time = 1e-3
+    num_integrations = Int(run_time / integration_time)
+    l1_num_samples = Int(run_time * l1_sample_freq)
+    l5_num_samples = Int(run_time * l5_sample_freq)
+    l1_integration_samples = Int(integration_time * l1_sample_freq)
+    l5_integration_samples = Int(integration_time * l5_sample_freq)
+    
+    l1_carrier = cis.(2 * π * (l1_interm_freq + l1_doppler) / l1_sample_freq * (1:l1_num_samples) + l1_carrier_phase)
+    l5_carrier = cis.(2 * π * (l5_interm_freq + l5_doppler) / l5_sample_freq * (1:l5_num_samples) + l5_carrier_phase)
+    l1_sampled_code = gen_code(1:l1_num_samples, l1_code_doppler + l1_code_freq, l1_code_phase, l1_sample_freq, SATELLITE_1_CODE)
+    l5_sampled_code = gen_code(1:l5_num_samples, l5_code_doppler + l5_code_freq, l5_code_phase, l5_sample_freq, L5_SAT1_CODE)
+    l1_signal = l1_carrier .* l1_sampled_code
+    l5_signal = l5_carrier .* l5_sampled_code
+
+    l1_results = Tracking.TrackingResults(0, l1_carrier_phase, 0.0, l1_code_phase, [0.0 + 0.0im])
+    l5_results = Tracking.TrackingResults(0, l5_carrier_phase, 0.0, l5_code_phase, [0.0 + 0.0im])
+
+    track = Tracking.init_joined_tracking([GPSL1(), GPSL5()], [l1_results, l5_results], [l1_sample_freq, l5_sample_freq], [l1_interm_freq, l5_interm_freq], 10.0, 1.0, 1)
+    
+    l1_code_phases = zeros(num_integrations)
+    l5_code_phases = zeros(num_integrations)
+    l1_calculated_code_phases =  mod.((1:num_integrations) * l1_integration_samples * (l1_code_doppler + l1_code_freq) / l1_sample_freq + l1_code_phase, 1023)
+    l5_calculated_code_phases = mod.((1:num_integrations) * l5_integration_samples * (l5_code_doppler + l5_code_freq) / l5_sample_freq + l5_code_phase, 10230)
+    l1_carrier_dopplers = zeros(num_integrations)
+    l5_carrier_dopplers = zeros(num_integrations)
+    l1_code_dopplers = zeros(num_integrations)
+    l5_code_dopplers = zeros(num_integrations)
+
+    joined_results = nothing
+    for i = 1:num_integrations
+        l1_current_signal = [1, 1].' .* l1_signal[l1_integration_samples * (i - 1) + 1:l1_integration_samples * i]# .+ complex.(randn(l1_integration_samples, 2), randn(l1_integration_samples, 2)) .* 10^(15 / 20)
+        l5_current_signal = [1, 1].' .* l5_signal[l5_integration_samples * (i - 1) + 1:l5_integration_samples * i]# .+ complex.(randn(l5_integration_samples, 2), randn(l5_integration_samples, 2)) .* 10^(15 / 20)
+        track, joined_results = track([l1_current_signal, l5_current_signal], beamform)
+        l1_code_phases[i] = joined_results[1].code_phase
+        l5_code_phases[i] = joined_results[2].code_phase
+        l1_carrier_dopplers[i] = joined_results[1].carrier_doppler
+        l5_carrier_dopplers[i] = joined_results[2].carrier_doppler
+        l1_code_dopplers[i] = joined_results[1].code_doppler
+        l5_code_dopplers[i] = joined_results[2].code_doppler
+    end
+
+    @test joined_results[1].carrier_doppler ≈ l1_doppler atol = 5e-2
+    @test joined_results[2].carrier_doppler ≈ l5_doppler atol = 5e-2
+    @test joined_results[1].code_phase ≈ l1_calculated_code_phases[end] atol = 5e-6
+    @test joined_results[2].code_phase ≈ l5_calculated_code_phases[end] atol = 5e-5
+    @test joined_results[1].code_doppler ≈ l1_code_doppler atol = 5e-5
+    @test joined_results[2].code_doppler ≈ l5_code_doppler atol = 5e-4
+
+    figure("L1 code_phases(b) and calculated (r)")
+    plot(l1_calculated_code_phases, color = "red")
+    plot(l1_code_phases, color = "blue")
+    figure("L5 code_phases")
+    plot(l5_calculated_code_phases, color = "red")
+    plot(mod.(l5_code_phases, 10230), color = "blue")
+    figure("L1 carrier_dopplers")
+    plot(l1_carrier_dopplers)
+    figure("L5 carrier_dopplers")
+    plot(l5_carrier_dopplers)
+    figure("L1 code dopplers")
+    plot(l1_code_dopplers)
+    figure("L5 code dopplers")
+    plot(l5_code_dopplers)
+end =#
