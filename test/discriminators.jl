010a9e9d55b0810a2ad6c59fdfcba4b227d342b7
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2018-12-21 17:12:16 +0100

    Add discriminators to test

--- test/discriminators.jl
+++ test/discriminators.jl
@@ -1,9 +1,9 @@
 
 @testset "PLL discriminator" begin
-    test_signal_prephase = [-0.5 + sqrt(3) / 2im, -1 + sqrt(3) * 1im, -0.5 + sqrt(3) / 2im]
-    test_signal = [0.5 + 0.0im, 1 + 0.0im, 0.5 + 0.0im]
-    test_signal_postphase = [0.5 + sqrt(3) / 2im, 1 + sqrt(3) * 1im, 0.5 + sqrt(3) / 2im]
-    
+    test_signal_prephase = @SVector [-0.5 + sqrt(3) / 2im, -1 + sqrt(3) * 1im, -0.5 + sqrt(3) / 2im]
+    test_signal = @SVector [0.5 + 0.0im, 1 + 0.0im, 0.5 + 0.0im]
+    test_signal_postphase = @SVector [0.5 + sqrt(3) / 2im, 1 + sqrt(3) * 1im, 0.5 + sqrt(3) / 2im]
+
     @test @inferred(Tracking.pll_disc(test_signal_prephase)) == -π / 3  #-60°
     @test @inferred(Tracking.pll_disc(test_signal)) == 0
     @test @inferred(Tracking.pll_disc(test_signal_postphase)) == π / 3  #+60°
@@ -11,12 +11,12 @@ end
 
 
 @testset "DLL discriminator" begin
-    test_signal_very_early = [0. + 0.0im, 0.5 + 0.0im, 1.0 + 0.0im] #τ = 0.5
-    test_signal_early = [0.25 + 0.0im, 0.75 + 0.0im, 0.75 + 0.0im] #τ = 0.25
-    test_signal_in_time = [0.5 + 0.0im, 1 + 0.0im, 0.5 + 0.0im] #τ = 0
-    test_signal_late = [0.75 + 0.0im, 0.75 + 0.0im, 0.25 + 0.0im] #τ = -0.25
-    test_signal_very_late = [1.0 + 0.0im, 0.5 + 0.0im, 0.0 + 0.0im] #τ = -0.5
-    
+    test_signal_very_early = @SVector [0. + 0.0im, 0.5 + 0.0im, 1.0 + 0.0im] #τ = 0.5
+    test_signal_early = @SVector [0.25 + 0.0im, 0.75 + 0.0im, 0.75 + 0.0im] #τ = 0.25
+    test_signal_in_time = @SVector [0.5 + 0.0im, 1 + 0.0im, 0.5 + 0.0im] #τ = 0
+    test_signal_late = @SVector [0.75 + 0.0im, 0.75 + 0.0im, 0.25 + 0.0im] #τ = -0.25
+    test_signal_very_late = @SVector [1.0 + 0.0im, 0.5 + 0.0im, 0.0 + 0.0im] #τ = -0.5
+
     @test @inferred(Tracking.dll_disc(test_signal_very_early)) == 0.5
     @test @inferred(Tracking.dll_disc(test_signal_early)) == 0.25
     @test @inferred(Tracking.dll_disc(test_signal_in_time)) == 0
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test, Tracking, GNSSSignals, LinearAlgebra, DataStructures, StaticArrays
 import Unitful: Hz, s, ms
 
-#include("discriminators.jl")
+include("discriminators.jl")
 include("loop_filters.jl")
 include("tracking_loop.jl")
