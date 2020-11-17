03c52ee79789a7f6bd24af3b83770891df3ba743
Michael Niestroj <michael.niestroj@nav.rwth-aachen.de>
2020-11-17 15:41:42 +0100

    Added required functions for GenericBOC signals

---
+++ src/generic_boc.jl
@@ -0,0 +1,19 @@
+"""
+$(SIGNATURES)
+
+Checks if upcoming integration is a new bit for generic BOC signal
+"""
+function is_upcoming_integration_new_bit(
+    ::Type{GenericBOC{T}}, 
+    prns,
+    num_prns
+) where T<:AbstractGNSSSystem
+    is_upcoming_integration_new_bit(T,prns,num_prns)
+end
+
+function get_default_correlator(
+    ::Type{GenericBOC{T}}, 
+    numAnts::NumAnts{N}
+) where {N, T<:AbstractGNSSSystem}
+    EarlyPromptLateCorrelator(numAnts)
+end
