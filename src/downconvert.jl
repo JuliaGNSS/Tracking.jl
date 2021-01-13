5e5211737fb81ef497d282a961e74b19c5fada8c
Can Ozmaden <can.oezmaden@rwth-aachen.de>
2021-01-13 13:15:24 +0100

    Implement GPU StructArray downconvert!

--- src/downconvert.jl
+++ src/downconvert.jl
@@ -1,13 +1,13 @@
 function downconvert!(
-    downconverted_signal_re::AbstractVector,
-    downconverted_signal_im::AbstractVector,
-    carrier_re::AbstractVector,
-    carrier_im::AbstractVector,
-    signal_re::AbstractVector,
-    signal_im::AbstractVector,
+    downconverted_signal_re::Vector{T},
+    downconverted_signal_im::Vector{T},
+    carrier_re::Vector{T},
+    carrier_im::Vector{T},
+    signal_re::Vector{T},
+    signal_im::Vector{T},
     start_sample::Integer,
     num_samples_left::Integer
-)
+) where T <: AbstractFloat
     @avx unroll = 3 for i = start_sample:num_samples_left + start_sample - 1
         downconverted_signal_re[i] = signal_re[i] * carrier_re[i] +
             signal_im[i] * carrier_im[i]
@@ -17,15 +17,15 @@ function downconvert!(
 end
 
 function downconvert!(
-    downconverted_signal_re::AbstractMatrix,
-    downconverted_signal_im::AbstractMatrix,
-    carrier_re::AbstractVector,
-    carrier_im::AbstractVector,
-    signal_re::AbstractMatrix,
-    signal_im::AbstractMatrix,
+    downconverted_signal_re::Matrix{T},
+    downconverted_signal_im::Matrix{T},
+    carrier_re::Vector{T},
+    carrier_im::Vector{T},
+    signal_re::Matrix{T},
+    signal_im::Matrix{T},
     start_sample::Integer,
     num_samples_left::Integer
-)
+) where T <: AbstractFloat
     @avx unroll = 3 for i = start_sample:num_samples_left + start_sample - 1, j = 1:size(signal_re, 2)
         # Calculate signal * carrier'
         downconverted_signal_re[i, j] = signal_re[i, j] * carrier_re[i] +
@@ -36,20 +36,20 @@ function downconvert!(
 end
 
 # StructArray GPU downconvert function Ants = 1
-# function downconvert!(
-#     downconverted_signal_re::CuMatrix{Complex{T}},
-#     downconverted_signal_im::CuMatrix{Complex{T}},
-#     carrier_re::CuVector{Complex{T}},
-#     carrier_im::CuVector{Complex{T}},
-#     signal_re::CuMatrix{Complex{T}},
-#     signal_im::CuMatrix{Complex{T}},
-#     start_sample::Integer,
-#     num_samples_left::Integer
-# ) where T <: AbstractFloat
-#     idxs = start_sample:start_sample + num_samples_left - 1
-#     @. @views downconverted_signal_re[idxs] = signal_re[idxs] * carrier_re[idxs] + signal_im[idxs] * carrier_im[idxs]
-#     @. @views downconverted_signal_im[idxs] = signal_im[idxs] * carrier_re[idxs] - signal_re[idxs] * carrier_im[idxs]
-# end
+function downconvert!(
+    downconverted_signal_re::CuVector{T},
+    downconverted_signal_im::CuVector{T},
+    carrier_re::CuVector{T},
+    carrier_im::CuVector{T},
+    signal_re::CuVector{T},
+    signal_im::CuVector{T},
+    start_sample::Integer,
+    num_samples_left::Integer
+) where {T <: AbstractFloat}
+    sample_range = start_sample:start_sample + num_samples_left - 1
+    @. @views downconverted_signal_re[sample_range] = signal_re[sample_range] * carrier_re[sample_range] + signal_im[sample_range] * carrier_im[sample_range]
+    @. @views downconverted_signal_im[sample_range] = signal_im[sample_range] * carrier_re[sample_range] - signal_re[sample_range] * carrier_im[sample_range]
+end
 
 # StructArray GPU downconvert function Ants > 1
 # function downconvert!(
