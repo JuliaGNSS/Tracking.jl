480f283f0165071a5049c11ed34996a437ea1f30
Can Ozmaden <can.oezmaden@rwth-aachen.de>
2021-10-29 21:10:17 +0200

    GPU BOC testset

---
+++ test/cuda/boc.jl
@@ -0,0 +1,18 @@
+@testset "CUDA: BOCcos" begin
+    system = BOCcos(GPSL1(use_gpu = Val(true)), 1, 1)
+    @test @inferred(Tracking.is_upcoming_integration_new_bit(system, 0xfffff00000, 50)) == true
+    @test @inferred(Tracking.is_upcoming_integration_new_bit(system, 0xfffff, 10)) == false
+    @test @inferred(Tracking.is_upcoming_integration_new_bit(system, 0xfffff, 40)) == true
+
+    @test @inferred(Tracking.get_default_correlator(system, NumAnts(1))) == EarlyPromptLateCorrelator(NumAnts(1))
+    @test @inferred(Tracking.get_default_correlator(system, NumAnts(3))) == EarlyPromptLateCorrelator(NumAnts(3))
+
+    system = BOCcos(GPSL5(use_gpu = Val(true)), 1, 1)
+    @test @inferred(Tracking.is_upcoming_integration_new_bit(system, 0x35, 50)) == true
+    @test @inferred(Tracking.is_upcoming_integration_new_bit(system, 0x35, 5)) == false
+    @test @inferred(Tracking.is_upcoming_integration_new_bit(system, 0x3ca, 10)) == true # 0x3ca == 1111001010
+
+    @test @inferred(Tracking.get_default_correlator(system, NumAnts(1))) == EarlyPromptLateCorrelator(NumAnts(1))
+    @test @inferred(Tracking.get_default_correlator(system, NumAnts(3))) == EarlyPromptLateCorrelator(NumAnts(3))
+
+end
