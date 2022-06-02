21121cc5b7e43efb3701edaa4894df48ab5548f3
Michael Niestroj <michael.niestroj@nav.rwth-aachen.de>
2022-06-02 11:05:37 +0200

    Code styling

--- src/Tracking.jl
+++ src/Tracking.jl
@@ -1,53 +1,45 @@
 module Tracking
-    using
-        DocStringExtensions,
-        GNSSSignals,
-        StaticArrays,
-        TrackingLoopFilters,
-        StructArrays,
-        LoopVectorization,
-        CUDA
+
+    using CUDA
+    using DocStringExtensions
+    using GNSSSignals
+    using LoopVectorization
+    using StaticArrays
+    using StructArrays
+    using TrackingLoopFilters
 
     using Acquisition: AcquisitionResults
     using Unitful: upreferred, Hz, dBHz, ms
     import Base.zero, Base.length, Base.resize!, LinearAlgebra.dot
 
-    export
-        get_early,
-        get_prompt,
-        get_late,
-        get_correlator,
-        get_accumulators,
-        get_num_accumulators,
-        get_early_index,
-        get_prompt_index,
-        get_late_index,
-        get_accumulator,
-        get_carrier_doppler,
-        get_carrier_phase,
-        get_code_doppler,
-        get_code_phase,
-        get_correlator_sample_shifts,
-        get_early_late_sample_spacing,
-        get_early_late_index_shift,
-        get_secondary_code_or_bit_found,
-        get_correlator_carrier_phase,
-        get_correlator_carrier_frequency,
-        get_state,
-        get_system,
-        get_cn0,
-        track,
-        TrackingState,
-        NumAnts,
-        NumAccumulators,
-        MomentsCN0Estimator,
-        AbstractCN0Estimator,
-        get_bits,
-        get_num_bits,
-        EarlyPromptLateCorrelator,
-        #VeryEarlyPromptLateCorrelator,
-        SecondaryCodeOrBitDetector,
-        GainControlledSignal
+    export get_early, get_prompt, get_late
+    export get_early_index,get_prompt_index, get_late_index
+    export get_correlator
+    export get_accumulator, get_accumulators
+    export get_num_accumulators
+    export get_carrier_doppler, get_carrier_phase
+    export get_code_doppler, get_code_phase
+    export get_correlator_sample_shifts
+    export get_early_late_sample_spacing
+    export get_early_late_index_shift
+    export get_secondary_code_or_bit_found
+    export get_correlator_carrier_phase, get_correlator_carrier_frequency
+    export get_state
+    export get_system
+    export get_cn0
+    export track
+    export get_bits
+    export get_num_bits
+
+    export TrackingState
+    export NumAnts
+    export NumAccumulators
+    export MomentsCN0Estimator
+    export AbstractCN0Estimator
+    export EarlyPromptLateCorrelator
+    #export VeryEarlyPromptLateCorrelator
+    export SecondaryCodeOrBitDetector
+    export GainControlledSignal
 
     struct NumAnts{x}
     end
