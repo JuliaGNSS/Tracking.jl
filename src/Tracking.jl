5af2c1bc109dde54b5bf7c9259cb72143b70df72
Ignatius Rivaldi <minecraft2048@gmail.com>
2023-07-20 17:53:51 +1000

    Add precompile statements

--- src/Tracking.jl
+++ src/Tracking.jl
@@ -104,4 +104,27 @@ module Tracking
     include("conventional_pll_and_dll.jl")
     include("tracking_state.jl")
     include("track.jl")
+    #include("downconvert_and_correlate.jl")
+
+
+    function _precompile_()
+        ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+        Base.precompile(Tuple{typeof(correlate),EarlyPromptLateCorrelator{ComplexF64, SVector{3, ComplexF64}},StructVector{ComplexF64, NamedTuple{(:re, :im), Tuple{Vector{Float64}, Vector{Float64}}}, Int64},Vector{Int16},SVector{3, Int64},Int64,Int64})   # time: 1.5417445
+        Base.precompile(Tuple{typeof(downconvert!),StructVector{ComplexF64, NamedTuple{(:re, :im), Tuple{Vector{Float64}, Vector{Float64}}}, Int64},Vector{ComplexF64},StructVector{ComplexF64, NamedTuple{(:re, :im), Tuple{Vector{Float64}, Vector{Float64}}}, Int64},Int64,Int64})   # time: 1.2596707
+        Base.precompile(Tuple{Type{DownconvertedSignalCPU},NumAnts{1}})   # time: 0.8288681
+        Base.precompile(Tuple{typeof(apply),Function,EarlyPromptLateCorrelator{ComplexF64, SVector{3, ComplexF64}}})   # time: 0.34806398
+        Base.precompile(Tuple{typeof(get_default_correlator),GPSL1,NumAnts{1}})   # time: 0.11166891
+        Base.precompile(Tuple{typeof(update),MomentsCN0Estimator{20},ComplexF64})   # time: 0.0758795
+        Base.precompile(Tuple{Type{MomentsCN0Estimator},Int64})   # time: 0.037634965
+        Base.precompile(Tuple{typeof(normalize),EarlyPromptLateCorrelator{ComplexF64, SVector{3, ComplexF64}},Int64})   # time: 0.009487625
+        Base.precompile(Tuple{Type{SecondaryCodeOrBitDetector}})   # time: 0.006050622
+        Base.precompile(Tuple{typeof(zero),EarlyPromptLateCorrelator{ComplexF64, SVector{3, ComplexF64}}})   # time: 0.00456108
+        Base.precompile(Tuple{typeof(get_prompt_index),SVector{3, Int64}})   # time: 0.003360711
+        Base.precompile(Tuple{typeof(resize!),DownconvertedSignalCPU{StructVector{ComplexF32, NamedTuple{(:re, :im), Tuple{Vector{Float32}, Vector{Float32}}}, Int64}, StructVector{ComplexF64, NamedTuple{(:re, :im), Tuple{Vector{Float64}, Vector{Float64}}}, Int64}},Int64,Vector{ComplexF64}})   # time: 0.002367571
+        Base.precompile(Tuple{typeof(dll_disc),GPSL1{Matrix{Int16}},EarlyPromptLateCorrelator{ComplexF64, SVector{3, ComplexF64}},SVector{3, Int64},Int64,Float64})   # time: 0.001880155
+        Base.precompile(Tuple{typeof(apply),DefaultPostCorrFilter,EarlyPromptLateCorrelator{ComplexF64, SVector{3, ComplexF64}}})   # time: 0.00148882
+        Base.precompile(Tuple{typeof(find),GPSL1{Matrix{Int16}},SecondaryCodeOrBitDetector,ComplexF64})   # time: 0.001341121
+        Base.precompile(Tuple{Type{NumAnts},Int64})   # time: 0.001071939
+    end
+    
 end
