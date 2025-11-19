a50656d4a6e92d6591452574f3fa348098e5cd9a
Soeren Schoenbrod <soeren.schoenbrod@juliahub.com>
2025-11-19 10:52:05 +0100

    fix: properly export GPU types from extension to parent module
    - Declare global variables in Tracking module for GPU types
    - Use __init__ function in TrackingCUDAExt to assign types to parent module
    - GPU types (GPUSatDownconvertAndCorrelator, GPUSystemDownconvertAndCorrelator,
      GPUDownconvertAndCorrelator) and convert_code_to_texture_memory function
      are now accessible via Tracking.* when CUDA is loaded
    - Extension exports don't propagate to parent module, so explicit assignment
      via @eval in __init__ is required
    
    Fixes issue where GPU types existed in TrackingCUDAExt but were not
    accessible in the Tracking namespace despite export statements.

--- ext/TrackingCUDAExt/TrackingCUDAExt.jl
+++ ext/TrackingCUDAExt/TrackingCUDAExt.jl
@@ -12,13 +12,19 @@ using DocStringExtensions
 using Unitful: Hz
 import Adapt
 
-# Re-export GPU functionality to make it available when CUDA is loaded
-export GPUSatDownconvertAndCorrelator,
-       GPUSystemDownconvertAndCorrelator,
-       GPUDownconvertAndCorrelator,
-       convert_code_to_texture_memory
-
 # Include the GPU implementation
 include("downconvert_and_correlate_gpu.jl")
+
+# Make GPU types available in the parent Tracking module
+# In package extensions, export doesn't propagate to the parent module,
+# so we explicitly assign them to the parent module's namespace in __init__
+function __init__()
+    @eval Tracking begin
+        GPUSatDownconvertAndCorrelator = $GPUSatDownconvertAndCorrelator
+        GPUSystemDownconvertAndCorrelator = $GPUSystemDownconvertAndCorrelator
+        GPUDownconvertAndCorrelator = $GPUDownconvertAndCorrelator
+        convert_code_to_texture_memory = $convert_code_to_texture_memory
+    end
+end
 
 end
